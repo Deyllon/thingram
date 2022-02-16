12b144306f28efca18c1f2b58c83223c33d6e187
deyllon ramos <deyllonp986@gmail.com>
2022-02-16 20:04:31 -0300

    Lib Django-on-heroku

--- setup/settings.py
+++ setup/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
+import django_on_heroku
 import os
 from decouple import config
 from pathlib import Path
@@ -137,3 +138,5 @@ MESSAGE_TAGS = {
 }
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+django_on_heroku.settings(locals())
